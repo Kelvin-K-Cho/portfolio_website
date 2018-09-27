b4bd60142b0e468a8ab0277b9cf3a9dec4a99af8
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-09-27 11:24:12 -0700

    Added sidebar navigation links

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 $(function(){
 
+  $.extend($.scrollTo.defaults, {
+    axis: 'y',
+    duration: 800,
+    offset: -55
+  });
+
   $(`#contact-modal-send`).click( () => {
     $(`#contact-modal-form`).submit();
   });
+
+  $(`.navigation-link`).click( (event) => {
+    event.preventDefault();
+    let id = event.currentTarget.id;
+    let target = id.replace(`link`, `section`);
+    $(`body`).scrollTo(`#${target}`);
+    return false;
+  });
+
 
 });
