002442aa26c402a6ad93536fc8af87963a8f10ad
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-10-02 10:40:54 -0700

    Refactored scroll jquery.

--- js/scroll.js
+++ js/scroll.js
@@ -15,6 +15,16 @@ $(function(){
       return Math.max(0, t>0? Math.min(elH, h-t) : Math.min(b, h));
     };
 
+    let normal = true;
+
+    let checkSize = () => {
+      if (window.matchMedia("(min-width: 60em)").matches) {
+        normal = true;
+      } else {
+        normal = false;
+      }
+    };
+
     $(window).on("scroll", function(){
       if (inViewport( $('#home-section') ) > 0) {
         $("#home-profile").fadeIn(1000);
@@ -31,19 +41,21 @@ $(function(){
       }
     });
 
-    if (window.matchMedia("(min-width: 60em)").matches) {
-      $(window).on("scroll", function(){
-        if (inViewport( $('#technology-section') ) > 0) {
-          let delay = 0;
-          $(`.technology-item > i`).each( (index, element) => {
-            $(element).slideDown(delay);
-            delay += 100;
-          } );
-        } else {
-          $(`.technology-item > i`).hide();
-        }
-      });
-    }
+    $(window).on("scroll resize", function(){
+      checkSize();
+      if (!normal) {
+        return false;
+      }
+      if (inViewport( $('#technology-section') ) > 0) {
+        let delay = 0;
+        $(`.technology-item > i`).each( (index, element) => {
+          $(element).slideDown(delay);
+          delay += 100;
+        } );
+      } else {
+        $(`.technology-item > i`).hide();
+      }
+    });
 
     $(`.navigation-link`).click( (event) => {
       event.preventDefault();
